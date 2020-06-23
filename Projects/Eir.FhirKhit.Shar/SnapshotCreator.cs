4160b19ce03875a2273b1641528275ea150aa980
Kurt W. Allen <KurtA@Gjoll.com>
2020-06-23 16:01:25 -0500

    Revert "Split into versioned and non"
    This reverts commit 5504b858189e4db7f88c9e86d8e13bb822b0a452.

---
+++ Projects/Eir.FhirKhit.Shar/SnapshotCreator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Specification.Snapshot;
+using Hl7.Fhir.Specification.Source;
+
+#if FHIR_R4
+namespace FhirKhit.Tools.R4
+#elif FHIR_R3
+namespace FhirKhit.Tools.R3
+#endif
+{
+    /// <summary>
+    /// Create the snapshot section in a StructureDefinition.
+    /// </summary>
+    public static class SnapshotCreator
+    {
+        public static async System.Threading.Tasks.Task CreateAsync(StructureDefinition structDef)
+        {
+            using (var tokenSource = new CancellationTokenSource())
+            {
+                var token = tokenSource.Token;
+                var t = System.Threading.Tasks.Task.Run(() =>
+               {
+                   Create(structDef);
+               }, token);
+                await t.ConfigureAwait(false);
+            }
+        }
+
+        public static void Create(StructureDefinition structDef)
+        {
+            if (structDef is null)
+                throw new ArgumentNullException(nameof(structDef));
+
+            structDef.Snapshot = new StructureDefinition.SnapshotComponent();
+
+            // Generate StructureDefinition.Snapshot using c# API.
+            SnapshotGeneratorSettings settings = SnapshotGeneratorSettings.CreateDefault();
+            SnapshotGenerator generator = new SnapshotGenerator(ZipFhirSource.Source, settings);
+            generator.Update(structDef);
+        }
+    }
+}
