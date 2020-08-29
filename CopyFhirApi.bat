574b44b31852478359c6fdf63d0546c1aade6d4f
Kurt W. Allen <KurtA@Gjoll.com>
2020-08-29 14:09:10 -0500

    Link directly to fhir.api libraries

---
+++ CopyFhirApi.bat
@@ -0,0 +1,6 @@
+     C:\Development\Tools\Eir.FhirKhit
+    
+copy ..\..\HL7\fhir-net-api\src\Hl7.Fhir.Core\bin\Debug\netstandard2.0\*.dll .\Library
+copy ..\..\HL7\fhir-net-api\src\Hl7.Fhir.Specification\bin\Debug\netstandard2.0\*.dll .\Library
+copy ..\..\HL7\fhir-net-api\src\Hl7.Fhir.Specification\bin\Debug\netstandard2.0\*.zip .\Library
+pause
--- Projects/Eir.FhirKhit.Shar/SnapshotCreator.cs
+++ Projects/Eir.FhirKhit.Shar/SnapshotCreator.cs
@@ -8,9 +8,29 @@ using Hl7.Fhir.Specification.Source;
 
 #if FHIR_R4
 namespace Eir.FhirKhit.R4
+{
+    /// <summary>
+    /// Create the snapshot section in a StructureDefinition.
+    /// </summary>
+    public static class SnapshotCreator
+    {
+        public static async System.Threading.Tasks.Task CreateAsync(StructureDefinition structDef)
+        {
+            if (structDef is null)
+                throw new ArgumentNullException(nameof(structDef));
+
+            structDef.Snapshot = new StructureDefinition.SnapshotComponent();
+
+            // Generate StructureDefinition.Snapshot using c# API.
+            SnapshotGeneratorSettings settings = SnapshotGeneratorSettings.CreateDefault();
+            SnapshotGenerator generator = new SnapshotGenerator(ZipFhirSource.Source, settings);
+            await generator.UpdateAsync(structDef).ConfigureAwait(true);
+        }
+    }
+}
+
 #elif FHIR_R3
 namespace Eir.FhirKhit.R3
-#endif
 {
     /// <summary>
     /// Create the snapshot section in a StructureDefinition.
@@ -44,3 +64,4 @@ namespace Eir.FhirKhit.R3
         }
     }
 }
+#endif
